a22280fafc45d37ce95bf8b74e4c83034d758128
Lucas4lves <zombnfl@gmail.com>
2025-10-29 15:06:14 -0300

    chore(project structure): removing projects folder

--- projects/prometheus-test-server/main.tf
+++
@@ -1,6 +0,0 @@
-module "prometheus-test-server" {
-  source                        = "./modules/test-server"
-  test_server_availability_zone = var.test_server_availability_zone
-  test_server_instance_type     = var.test_server_instance_type
-  test_server_tags              = var.test_server_tags
-}
